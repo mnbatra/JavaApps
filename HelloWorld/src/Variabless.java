5389948119774862966d0dc886fa2878b823e2a8
AB <yourbatra@gmail.com>
2020-04-18 22:26:39 +0530

    Expressions

---
+++ HelloWorld/src/Variabless.java
@@ -0,0 +1,10 @@
+public class Variabless {
+    public static void main() {
+        int myFirst = (10+5) + (2*10);
+        int mySecond = 12;
+        int myThird = myFirst*2;
+        int myTotal = myFirst+mySecond+myThird;
+        System.out.println("myTotal\n");
+        System.out.println(1000-myTotal);
+    }
+}
